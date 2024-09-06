1c53acd5bf99d1b1557b3df42bfc86d90c596065
Mohammed Ben Cheikh <bencheikh.official@gmail.com>
2024-09-06 02:41:22 +0100

    Add files via upload

---
+++ mini project 2/main.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct {
+    char nom[100];
+    char nt[100];
+    char am[100];
+}contact;
+
+      contact carnet[100];
+      int c=0,choi , i;
+      char modifier_nom[50];
+      char modifier_nt[50];
+      char modifier_am[50];
+
+    void ajouter(){
+          if (c<100){
+            printf("       =====>>  Entre le Nom de Contact ---------------- : ");
+            scanf("%s",carnet[c].nom);
+            printf("\n");
+            printf("       =====>>  Entre le Nemuro de telephon de Contact - : ");
+            scanf("%s",carnet[c].nt);
+            printf("\n");
+            printf("       =====>>  Entre le Mail de Contact --------------- : ");
+            scanf("%s",carnet[c].am);
+            c++;
+                    }
+                 }
+
+    void afficher(){
+        if (c>=1){
+        for ( i = 0; i<c;i++){
+            printf("\n\n");
+            printf("=====>> Contact numero %d : \n\n          Le nom du contact est                : %s" ,i+1,carnet[i].nom );
+            printf("\n\n");
+            printf("          Le nemuro de telephon de Contact est : %s",carnet[i].nt );
+            printf("\n\n");
+            printf("          Le mail de Contact est               : %s",carnet[i].am );
+            printf("\n\n");
+                             }
+                 }else printf("       =====>> Aucune contact enregistrer !!!");
+
+                   }
+
+
+    void supprimer(char nom[]){
+       int a=-1;
+       for(int i = 0 ; i<c;i++) {
+        if(strcmp(nom,carnet[i].nom)==0)
+        {
+            a=i;
+            break;
+        }
+                                }
+        if(a!=-1){
+            for(int i=a;i<c-1;i++) {
+                carnet[i]= carnet[i+1];
+                                   }
+            c--;
+                 }
+        else printf("       =====>> Contact Non Trouve !!!\n");
+                              }
+
+    void rechercher(char nom[]){
+
+       for( i = 0 ; i<c;i++){
+        if(strcmp(nom,carnet[i].nom)==0){
+            printf("                   =====>> Le nom de contact est                : %s \n",carnet[i].nom);
+            printf("\n");
+            printf("                   =====>> Le Nemuro de telephon de contact est : %s \n",carnet[i].nt);
+            printf("\n");
+            printf("                   =====>> Entre le mail de Contact est         : %s \n",carnet[i].am);
+            break;
+                                        }
+        else printf("       =====>> Contact Non Trouve !!!\n");
+                            }
+                               }
+
+    void modifier(){
+        if (c>=1){
+           printf("       =====>> Entre Le Nom De Contact : ");
+           scanf("%s",modifier_nom);
+           for(int i=0 ;i<c;i++){
+        if(strcmp(carnet[i].nom,modifier_nom)==0){
+         printf("entrer nouveu TELL :");
+         scanf("%s",modifier_nt);
+         printf("entrer nouveu EMAIL :" );
+         scanf("%s",modifier_am);
+
+         strcpy(carnet[i].nt,modifier_nt);
+         strcpy(carnet[i].am,modifier_am);
+                                                 }
+                                }
+                 }
+          else printf("       =====>> Aucune contact enregistrer !!!");
+
+                   }
+
+
+
+
+int main ()
+{
+    char nom [50];
+    int choix =0;
+    do
+    {
+        printf("\n\n");
+printf("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+printf("\n");printf("\n");
+printf("     ----------------- Systeme de Gestion de Contacts -----------------");
+printf("\n");printf("\n");
+printf("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+printf("|\n");printf("       |\n");printf("       |\n");
+
+printf("       |===>> Pour Ajouter un Contact entre -------------------------- : 1\n       |\n");
+printf("       |===>> Pour Afficher tous les Contacts disponibles entre ------ : 2\n       |\n");
+printf("       |===>> Pour Rechercher un Contact par son titre entre ----------: 3\n       |\n");
+printf("       |===>> Pour Mettre a jour les donne d'un Contact entre -------- : 4\n       |\n");
+printf("       |===>> Pour Supprimer un Contact du stock entre --------------- : 5\n        \n");
+printf("\n\n");
+printf("                 ===>> entre votre numero ici : ");
+scanf("%d",&choi);
+printf("\n\n");
+
+
+
+        switch (choi)
+        {
+        case 1:
+           ajouter();
+            break;
+        case 2:
+            afficher();
+            break;
+        case 3:
+            if (c>=1){
+            printf("       =====>> Donner le nom de contact a chercher : ");
+            scanf("%s",nom);
+            printf("\n\n");
+            rechercher(nom);
+            }else printf("       =====>> Aucune contact enregistrer !!!");
+            break;
+        case 4:
+            modifier();
+            break;
+
+        case 5:
+            if (c>=1){
+            printf("       =====>> Donner le nom de contact a supprimer : ");
+            scanf("%s",nom);
+            supprimer(nom);
+            }else printf("       =====>> Aucune contact enregistrer !!!");
+            break;
+            default:
+
+            printf("       =====>> Entre un valeur entre <1> et <5> !!! \n");
+             break;
+        }
+
+    }while(choi =! 0);
+
+   return 0;
+}
+
